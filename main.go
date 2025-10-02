3f835e0ad744f70db72f98ec7f43e65ea02e0b4e
zshuzh <40901142+zshuzh@users.noreply.github.com>
2025-10-02 19:12:08 +0100

    fix: rename Init to Configure to match command

--- main.go
+++ main.go
@@ -10,10 +10,10 @@ import (
 )
 
 type Wt struct {
-	Version kong.VersionFlag  `short:"v" help:"Print version information"`
-	Init    configure.Options `cmd:"" help:"Start the wt setup wizard"`
-	List    list.Options      `cmd:"" help:"List current worktrees"`
-	Switch  switchcmd.Options `cmd:"" help:"Switch to a worktree"`
+	Version   kong.VersionFlag  `short:"v" help:"Print version information"`
+	Configure configure.Options `cmd:"" help:"Start the wt setup wizard"`
+	List      list.Options      `cmd:"" help:"List current worktrees"`
+	Switch    switchcmd.Options `cmd:"" help:"Switch to a worktree"`
 }
 
 func main() {

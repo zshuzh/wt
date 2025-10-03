07fa7434d1ee5b617c0272eae2bb87ca185595ed
zshuzh <40901142+zshuzh@users.noreply.github.com>
2025-10-03 10:21:10 +0100

    feat: implement config file with default path and hooks

--- add/command.go
+++ add/command.go
@@ -1,20 +1,37 @@
 package add
 
 import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+
 	"github.com/charmbracelet/huh"
+	"github.com/zshuzh/wt/internal/config"
 	"github.com/zshuzh/wt/internal/git"
 )
 
 type Options struct{}
 
 func (o Options) Run() error {
+	cfg, err := config.Load()
+	if err != nil {
+		return err
+	}
+
 	root, err := git.GetRepoRoot()
 	if err != nil {
 		return err
 	}
 
-	path := root
+	var path string
 	var branch string
+
+	if cfg.PathDefault != "" {
+		path = filepath.Join(root, cfg.PathDefault)
+	} else {
+		path = root
+	}
 
 	branches, err := git.GetBranches()
 	if err != nil {
@@ -45,8 +62,38 @@ func (o Options) Run() error {
 		return err
 	}
 
-	return git.AddWorktree(git.Worktree{
+	err = git.AddWorktree(git.Worktree{
 		Path:   path,
 		Branch: branch,
 	})
+	if err != nil {
+		return err
+	}
+
+	for _, hook := range cfg.Hooks {
+		if hook.Root != root {
+			continue
+		}
+
+		workDir := filepath.Join(path, hook.Subdir)
+
+		shell := os.Getenv("SHELL")
+		if shell == "" {
+			shell = "/bin/sh"
+		}
+
+		// The -l flag sources login files (e.g. ~/.zshrc) to set up a full environment. This also
+		// allows wt to set env variables that hooks can use (e.g. $GIT_REPO_ROOT).
+		cmd := exec.Command(shell, "-l", "-c", hook.Command)
+		cmd.Dir = workDir
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		cmd.Stdin = os.Stdin
+
+		if err := cmd.Run(); err != nil {
+			fmt.Printf("Hook failed: %v\n", err)
+		}
+	}
+
+	return nil
 }
---
+++ internal/config/config.go
@@ -0,0 +1,68 @@
+package config
+
+import (
+	"encoding/json"
+	"errors"
+	"os"
+	"path/filepath"
+)
+
+type Hook struct {
+	Root    string `json:"root"`
+	Command string `json:"command"`
+	Subdir  string `json:"subdir"`
+}
+
+type Config struct {
+	PathDefault string `json:"pathDefault"`
+	Hooks       []Hook `json:"hooks"`
+}
+
+func Load() (*Config, error) {
+	configPath, err := GetConfigPath()
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := os.ReadFile(configPath)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return &Config{}, nil
+		}
+		return nil, err
+	}
+
+	var cfg Config
+	if err := json.Unmarshal(data, &cfg); err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
+}
+
+func Save(cfg *Config) error {
+	configPath, err := GetConfigPath()
+	if err != nil {
+		return err
+	}
+
+	dir := filepath.Dir(configPath)
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return err
+	}
+
+	data, err := json.MarshalIndent(cfg, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(configPath, data, 0644)
+}
+
+func GetConfigPath() (string, error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	return filepath.Join(home, ".config", "wt", "config.json"), nil
+}
